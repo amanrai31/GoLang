9828017241b65c8952c00abf68eb436d9528f25e
amanrai31 <aman970rai@gmail.com>
2024-10-02 17:46:42 +0530

    Golang

---
+++ 01hello/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(2 + 2) // Print and Println
+
+	fmt.Println("hello")
+
+	var confernceName = "Go conference"
+
+	fmt.Println("working on", confernceName)
+
+}
